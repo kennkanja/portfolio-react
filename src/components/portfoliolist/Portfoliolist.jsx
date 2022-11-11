f237128fdf671f3b68584a87f6af1fc999d9ef8c
kennkanja <kennedy.karanja2@gmail.com>
2022-11-11 12:44:55 +0300

    first commit

---
+++ src/components/portfoliolist/Portfoliolist.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./portfoliolist.scss"
+
+export default function Portfoliolist({id,title,active,setSelected}) {
+  return (
+    <li className={active ? "portfoliolist active" : "portfoliolist"}
+    onClick ={() => setSelected(id)}
+    >
+        
+        {title}
+    </li>
+  )
+}
